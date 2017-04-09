ff390da0df177cd8a20614682052aaf3ca93f381
Ye Jia <ye.jia.alice@gmail.com>
2017-04-09 15:00:26 +0800

    Query方法链查询风格

--- initmongoose/demo14.js
+++ initmongoose/demo14.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
 const T = mongoose.model('Topic', {
-  // arr: []
-  obj: {}
+  arr: [],
+  age: Number,
+  name: String
+  // obj: {}
 });
 
 function generatorArr(size) {
@@ -20,14 +22,14 @@ function generatorArr(size) {
 T.remove({}, function(){
   T.create([
     // {arr: [11, 22, 33]},
-    // {arr: [33, 44,55]},
+    // {arr: [33, 44,55], name: 'alice', age: 8},
     // {arr: [55, 66, 77, 88]}
 
-    // {arr: generatorArr(5)},
-    // {arr: generatorArr(10)},
-    // {arr: generatorArr(15)},
+    {arr: generatorArr(5)},
+    {arr: generatorArr(10)},
+    {arr: generatorArr(15)},
 
-    {obj: {name:'java', num: 11}},{obj: {name:'javascript', num:12}},{obj: {name:'node.js'}}
+    // {obj: {name:'java', num: 11}},{obj: {name:'javascript', num:12}},{obj: {name:'node.js'}}
   ]).then(function(){
     // T.find({arr: {$all: [33, 44]}}).then(function(result){
     //   console.log(result);
@@ -45,13 +47,21 @@ T.remove({}, function(){
     //   console.log(result);
     // });
 
-    T.find({
-      $where: function(){
-        // return this.obj.name === 'javascript';
-        return /^java\w*$/.test(this.obj.name) && this.obj.num === 12;
-      }
-    }).then(function(result){
+    // T.find({
+    //   $where: function(){
+    //     // return this.obj.name === 'javascript';
+    //     return /^java\w*$/.test(this.obj.name) && this.obj.num === 12;
+    //   }
+    // },null, {limit:1}).then(function(result){
+    //   console.log(result);
+    // });
+
+    // let query = T.find();
+    // query.where('arr').all([33, 44]).select('name age').exec(function(err, result){
+    //   console.log(result);
+    // });
+    T.find().where('arr').elemMatch({name: 'alice9'}).exec(function(err, result){
       console.log(result);
-    });
+    })
   });
 });
